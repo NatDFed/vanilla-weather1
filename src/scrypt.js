73f6ca46cf78be07eae5ec22127a1f504e152259
NatD <112499460+NatDFed@users.noreply.github.com>
2022-09-09 00:11:26 +0100

    Added elements

--- src/scrypt.js
+++ src/scrypt.js
@@ -2,9 +2,13 @@ function showTemp(response) {
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
 temperatureElement.innerHTML = Math.round (response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = Math.round (response.data.wind.speed);
 }
 let apiKey = "10566d0f9902e77e497ca722e7aa0b84";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Cork&appid=${apiKey}&units=metric`;
