befa749a6fa45e1aa496576a89881f52211a45a5
NatD <112499460+NatDFed@users.noreply.github.com>
2022-09-08 22:44:58 +0100

    Added a function

--- src/scrypt.js
+++ src/scrypt.js
@@ -1,3 +1,7 @@
+function showTemp(response) {
+console.log(response.data);
+}
 let apiKey = "10566d0f9902e77e497ca722e7aa0b84";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Cork&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
+axios.get(apiUrl).then(showTemp);
