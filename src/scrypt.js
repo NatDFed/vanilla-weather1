f272de3345a3c635a8def439c33a2bb435b727d6
NatD <112499460+NatDFed@users.noreply.github.com>
2022-09-08 23:57:42 +0100

    Added id

--- src/scrypt.js
+++ src/scrypt.js
@@ -1,8 +1,11 @@
 function showTemp(response) {
 let temperatureElement = document.querySelector("#temperature");
-temperatureElement.innerHtml = Math.round (response.data.main.temp);
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+temperatureElement.innerHTML = Math.round (response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
 }
 let apiKey = "10566d0f9902e77e497ca722e7aa0b84";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Cork&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
 axios.get(apiUrl).then(showTemp);
