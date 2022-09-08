f7452b5b2094383696aedd03402110c6ecf5fdee
NatD <112499460+NatDFed@users.noreply.github.com>
2022-09-08 23:36:03 +0100

    Added a Temp function

--- src/scrypt.js
+++ src/scrypt.js
@@ -1,5 +1,6 @@
 function showTemp(response) {
-console.log(response.data);
+let temperatureElement = document.querySelector("#temperature");
+temperatureElement.innerHtml = Math.round (response.data.main.temp);
 }
 let apiKey = "10566d0f9902e77e497ca722e7aa0b84";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Cork&appid=${apiKey}&units=metric`;
