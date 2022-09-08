ebe369a754b083891acd11b81e1b37e47153acc1
NatD <112499460+NatDFed@users.noreply.github.com>
2022-09-08 22:37:00 +0100

    ApiKey console logged

--- src/scrypt.js
+++ src/scrypt.js
@@ -1,2 +1,3 @@
 let apiKey = "10566d0f9902e77e497ca722e7aa0b84";
-let apiUrl =
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Cork&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
