5532c8393f1286cedcda0232cf514fbb6189dd00
NatD <112499460+NatDFed@users.noreply.github.com>
2022-09-11 22:14:18 +0100

    Changed attribute

--- src/scrypt.js
+++ src/scrypt.js
@@ -19,12 +19,14 @@ let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
+let iconElement = document.querySelector("#icon");
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
+iconElement.setAttribute("src",`http://openweathermap.org/img/wn/04d@2x.png`);
 }
 let apiKey = "10566d0f9902e77e497ca722e7aa0b84";
 let city = "Cork";
