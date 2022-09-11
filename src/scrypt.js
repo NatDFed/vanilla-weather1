e921366791d0f18238a2a1827f1c8d202781239f
NatD <112499460+NatDFed@users.noreply.github.com>
2022-09-11 21:27:29 +0100

    Updated date function

--- src/scrypt.js
+++ src/scrypt.js
@@ -1,14 +1,24 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let day = date.getDay();
+    return `${day} ${hours}:${minutes}`;
+}
+
 function showTemp(response) {
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
-temperatureElement.innerHTML = Math.round (response.data.main.temp);
+let dateElement = document.querySelector("#date");
+temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
-windElement.innerHTML = Math.round (response.data.wind.speed);
+windElement.innerHTML = Math.round(response.data.wind.speed);
+dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = "10566d0f9902e77e497ca722e7aa0b84";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Cork&appid=${apiKey}&units=metric`;
