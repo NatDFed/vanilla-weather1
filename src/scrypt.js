2a8ecb1a79c9aa5af167981ff3e25faee1544524
NatD <112499460+NatDFed@users.noreply.github.com>
2022-09-12 01:09:23 +0100

    Created a temperature variable

--- src/scrypt.js
+++ src/scrypt.js
@@ -40,7 +40,16 @@ function handleSubmit(event) {
     let cityInputElement = document.querySelector("#city-input");
     search(cityInputElement.value);
 }
+function showFahrTemp(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    let fahrTemp = (temperatureElement * 9) / 5 * 32;
+    temperatureElement.innerHTML = Math.round(fahrTemp);
+}
 search("Cork");
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrTemp);
