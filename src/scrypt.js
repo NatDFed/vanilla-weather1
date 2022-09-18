08a8cd1046ed1bf4632bf8dc78e5bdca622c760b
NatD <112499460+NatDFed@users.noreply.github.com>
2022-09-18 15:20:09 +0100

    Changed apiKey

--- src/scrypt.js
+++ src/scrypt.js
@@ -39,7 +39,7 @@ function showForecast(response) {
 
 function getForecast(coordinates) {
     console.log(coordinates);
-    let apiKey = "10566d0f9902e77e497ca722e7aa0b84";
+    let apiKey = "0a521eaf234a3a56f45252fac3c737ad";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(showForecast);
 }
