7e2ff6b79b5dac9482987de4d5a8b85d14e01362
NatD <112499460+NatDFed@users.noreply.github.com>
2022-09-18 14:28:27 +0100

    Corrected apiUrl

--- src/scrypt.js
+++ src/scrypt.js
@@ -40,7 +40,7 @@ function showForecast(response) {
 function getForecast(coordinates) {
     console.log(coordinates);
     let apiKey = "10566d0f9902e77e497ca722e7aa0b84";
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(showForecast);
 }
 
@@ -64,9 +64,6 @@ windElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
 iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt", response.data.weather[0].description);
-
-getForecast(response.data.coord);
-
 }
 function search(city) {
     let apiKey = "10566d0f9902e77e497ca722e7aa0b84";
